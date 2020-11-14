21cda91b06f4366736c7b53cb0703a4c0daa5ab0
mengsuenyan <mengsuenyan@163.com>
2020-11-14 17:05:30 +0800

    [ADD] add git-clone to clone some repositories from the speciefied link sets; [ADD] add update-git to update all repositories in the current directory

---
+++ gitclone.ps1
@@ -0,0 +1,109 @@
+﻿$wp = $PSScriptRoot
+Set-Location $wp
+
+$crpathsarr = @(
+'https://github.com/filecoin-project/venus.git',
+'https://github.com/filecoin-project/specs.git',
+'https://github.com/filecoin-project/FIPs.git',
+'https://github.com/filecoin-project/rust-fil-secp256k1.git',
+'https://github.com/filecoin-project/cpp-filecoin.git',
+'https://github.com/filecoin-project/go-data-transfer.git',
+'https://github.com/filecoin-project/tpm.git',
+'https://github.com/filecoin-project/neptune.git',
+'https://github.com/filecoin-project/lotus.git',
+'https://github.com/filecoin-project/slate.git',
+'https://github.com/filecoin-project/specs-actors.git',
+'https://github.com/filecoin-project/sentinel-visor.git',
+'https://github.com/filecoin-project/ent.git',
+'https://github.com/filecoin-project/filecoin-docs.git',
+'https://github.com/filecoin-project/rust-fil-proofs.git',
+'https://github.com/filecoin-project/rust-gpu-tools.git',
+'https://github.com/filecoin-project/filecoin-ffi.git',
+'https://github.com/filecoin-project/go-fil-markets.git',
+'https://github.com/filecoin-project/statediff.git',
+'https://github.com/filecoin-project/go-amt-ipld.git',
+'https://github.com/filecoin-project/bellperson.git',
+'https://github.com/filecoin-project/oni.git',
+'https://github.com/filecoin-project/rust-filecoin-proofs-api.git',
+'https://github.com/filecoin-project/go-jsonrpc.git',
+'https://github.com/filecoin-project/filecoin-discover-validator.git',
+'https://github.com/filecoin-project/devgrants.git',
+'https://github.com/filecoin-project/go-bitfield.git',
+'https://github.com/filecoin-project/go-hamt-ipld.git',
+'https://github.com/filecoin-project/rust-fil-nse-gpu.git',
+'https://github.com/filecoin-project/network-info.git',
+'https://github.com/filecoin-project/sentinel-drone.git',
+'https://github.com/filecoin-project/merkletree.git',
+'https://github.com/filecoin-project/phase2.git',
+'https://github.com/filecoin-project/paired.git',
+'https://github.com/filecoin-project/blstrs.git',
+'https://github.com/filecoin-project/mapr.git',
+'https://github.com/filecoin-project/blst.git',
+'https://github.com/filecoin-project/bls-signatures.git',
+'https://github.com/filecoin-project/rust-filbase.git',
+'https://github.com/filecoin-project/rust-sha2ni.git'
+)
+$crpathsarr = $crpathsarr | Select-Object -Unique
+$crpaths = @{}
+foreach ($idx in 0..($crpathsarr.Length-1)) {
+$crpaths.Add($idx, $crpathsarr[$idx])
+}
+
+function git-process {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $app,
+        [Parameter(Mandatory=$true)]
+        [string] $cmd
+    )
+
+    $process = new-object System.Diagnostics.Process
+    $process.StartInfo.FileName = $app
+    $process.StartInfo.Arguments = $cmd
+    $process.StartInfo.UseShellExecute = 0
+    $process.StartInfo.RedirectStandardInput = 0
+    $process.StartInfo.RedirectStandardOutput = 1
+    $process.StartInfo.RedirectStandardError = 1
+    $process.StartInfo.CreateNoWindow = 1
+    $process.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden;
+    $process.Start()
+    $error_info = $process.StandardError.ReadToEnd()
+    $result = $process.StandardOutput.ReadToEnd()
+	$result
+    $error_info
+}
+
+
+while ($true) {
+    $cr = (Get-ChildItem -Path $wp -Directory).BaseName
+    $isneedbreak = $true
+	Out-Default -Input $crpaths
+    $crhash = $crpaths.Clone()
+
+    foreach ($item in $crhash.GetEnumerator()) {
+        $crpath = $item.value
+        if ($crpath -match "https://github.com/.*\.git") {
+            $name = $crpath.SubString($crpath.lastindexof('/') + 1, $crpath.lastindexof('.') - $crpath.lastindexof('/') - 1)
+            if ($cr -notcontains $name) {
+                Write-Host $crpath
+                $info = git-process -app "git" -cmd "clone $crpath"
+				Out-Default -Input $info
+                if ($info -match 'not') {
+                    $crpaths.Remove($item.key)
+                } elseif ((-not ($info -match 'fatal|error')) -and ($info -match 'Cloning into')) {
+                    $crpaths.Remove($item.key)
+				}
+                Start-Sleep -Seconds 60
+                $isneedbreak = $false
+            } else {
+				$crpaths.Remove($item.key)
+			}
+        }
+    }
+
+    if ($isneedbreak) {
+        break
+    }
+}
+
+Read-Host "Enter any key exist"
---
+++ update-git.ps1
@@ -0,0 +1,63 @@
+﻿# usage: put the file in the root directory of git warehouse and run it.
+
+$NOT_GIT_REPO = "NotGitRepository"
+$UPD_FAILED = "UpdateFailed"
+$LESS_TIME = "OnlyUpdatedEveryHalfMonth"
+$UPD_SUCESS = "UpdateSucess"
+
+$OriginalPath = $PSScriptRoot
+
+$Repositories = (Get-ChildItem -Path $OriginalPath) | Sort-Object -Property LastWriteTime
+$Today = Get-Date
+
+$IsUpd = @{}
+foreach ($x in (1..1)) {
+	foreach ($repo in $Repositories) {
+		$path = $repo.FullName
+
+		if ($path -in $IsUpd) {
+			continue
+		}
+
+        if (($Today - $repo.LastWriteTime).Days -lt 15) {
+            $IsUpd[$path] = $LESS_TIME
+            continue
+        }
+
+	    if (-not ((Get-ChildItem -Path $path -Force) -match ".*\.git$")) {
+			$IsUpd[$path]=$NOT_GIT_REPO
+		    continue
+	    }
+
+		Set-Location -Path $path
+	    Get-Location | Out-Host
+		$out= $(git pull) *>&1
+
+		if (-not ($out -match ".*fatal.*")) {
+			$IsUpd[$path]=$UPD_SUCESS
+		}
+    
+		$out | Out-Host
+		Set-Location -Path $OriginalPath
+	}
+
+	if ($IsUpd.Count -ge $Repositories.Length) {
+		break
+	}
+}
+
+foreach ($repo in $Repositories) {
+	$path = $repo.FullName
+	if ($IsUpd[$path] -eq $UPD_SUCESS) {
+		Write-Host $repo,">",$UPD_SUCESS -ForegroundColor Green
+	} elseif ($IsUpd[$path] -eq $NOT_GIT_REPO) {
+		Write-Host $repo,">",$NOT_GIT_REPO -ForegroundColor White
+	} elseif ($IsUpd[$path] -eq $LESS_TIME) {
+		Write-Host $repo,">",$LESS_TIME -ForegroundColor Yellow
+    } else {
+		Write-Host $repo,">",$UPD_FAILED -ForegroundColor Red
+    }
+}
+
+Set-Location $OriginalPath
+Read-Host
